f07d01122ed9fd1e0d53c3c9872e2401c37c1ca1
Mael Gohaud <mael.gohaud@blacksales.co>
2017-05-29 09:40:22 +0000

    First

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const validator = require('bulk-email-verifier');
+const app = express();
+
+app.get('/', function (req, res) {
+  var domain = 'gmail.com';
+  var emails =  [
+    'teaast3@gmail.com',
+    'test1@gmail.com',
+    'test2@gmail.com',
+    'maelgohaud@gmail.com'
+];
+ 
+  validator.verifyEmails(domain, emails, {}, function(err, data){
+    console.log("Email Stats: ", err, data);	
+});
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  console.log("Serveur lancé");
+});
